9e75fb7ca35d202e264b7f952fef3ece2393d1ec
Elizabeth Klymova <lozaklimovaaadd@gmail.com>
2024-02-29 12:34:51 +0200

    route fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,7 +7,56 @@ const App = () => {
     <>
       <GlobalStyles />
       <Toaster position="top-center" />
+<<<<<<< Updated upstream
       <MainPage />
+=======
+
+      <ul>
+        <li>
+          <Link to="/">Welcome</Link>
+        </li>
+        <li>
+          <Link to="/auth">Auth</Link>
+        </li>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+      </ul>
+
+      <Suspense fallback="Loading">
+        <Routes>
+          <Route path="/home" element={<SharedLayout />}>
+            <Route
+              index
+              // element={
+              //   <PrivateRoute component={<HomePage />} redirectTo={'/auth'} />
+              // }
+              element={<HomePage />}
+            />
+            <Route
+              path=":boardId"
+              // element={
+              //   <PrivateRoute
+              //     component={<ScreensPage />}
+              //     redirectTo={'/auth'}
+              //   />
+              // }
+              element={<ScreensPage />}
+            />
+          </Route>
+
+          <Route path="/" element={<WelcomePage />} />
+          <Route
+            path="auth/:id"
+            element={
+              <PublicRoute component={<AuthPage />} redirectTo="/home" />
+            }
+          />
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+>>>>>>> Stashed changes
     </>
   );
 };
