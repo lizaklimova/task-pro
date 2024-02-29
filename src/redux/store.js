14ccd361d054892b053d5f23c53ceb56c97cf0d9
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-02-29 23:14:37 +0200

    rename with prefix 'auth'

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { authReducer } from './auth/slice';
+import { authReducer } from './auth/authSlice';
 
 const rootReducer = combineReducers({
   auth: authReducer,
