3bb93a94baa24dc34976bc842f94e41ec1801088
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-02-29 22:12:18 +0200

    add loginForm and Formik markup

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,57 @@
+import { Formik } from 'formik';
+import {
+  Background,
+  FormWrap,
+  AuthList,
+  AuthLink,
+  FormUi,
+  Input,
+  SubmitBtn,
+} from './LoginForm.styled';
+
+const LoginForm = () => {
+  const nandleFormSubmit = ms => new Promise(r => setTimeout(r, ms));
+
+  return (
+    <Background>
+      <FormWrap>
+        <AuthList>
+          <li>
+            <AuthLink to={`/auth/register`}>Registration</AuthLink>
+          </li>
+          <li>
+            <AuthLink to={`/auth/login`}>Log In</AuthLink>
+          </li>
+        </AuthList>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+          }}
+          onSubmit={async values => {
+            await nandleFormSubmit(500);
+            alert(JSON.stringify(values, null, 2));
+          }}
+        >
+          {({ isSubmitting }) => (
+            <FormUi>
+              <Input name="email" placeholder="Enter your email" type="email" />
+
+              <Input
+                name="password"
+                placeholder="Confirm a password"
+                type="password"
+              />
+
+              <SubmitBtn type="submit" disabled={isSubmitting}>
+                Log In Now
+              </SubmitBtn>
+            </FormUi>
+          )}
+        </Formik>
+      </FormWrap>
+    </Background>
+  );
+};
+
+export default LoginForm;
