68bfb44f5cf71fa8e259b35f9280ffe8c73e9b3d
Elizabeth Klymova <lozaklimovaaadd@gmail.com>
2024-03-01 17:37:56 +0200

    fix

--- src/components/Loader/Loader.styled.jsx
+++ src/components/Loader/Loader.styled.jsx
@@ -2,19 +2,19 @@ import styled from '@emotion/styled';
 import { RotatingLines } from 'react-loader-spinner';
 
 export const LoaderContainer = styled.div`
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   background-color: var(--background-color);
 `;
 
 export const StyledRotatingLines = styled(RotatingLines)`
-  stroke-color: var(--accent-color);
+  stroke: var(--accent-color);
   stroke-width: 5px;
   animation-duration: 0.75s;
 `;
