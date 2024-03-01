82343ac60b7d76ebaa60a9c2f8d01f2f3a66de74
dn280786lea <ekaterina.lubenets@gmail.com>
2024-03-01 13:46:53 +0100

    HelpModal
    HelpModal

--- src/components/NeedHelpModal/NeedHelpModal.jsx
+++ src/components/NeedHelpModal/NeedHelpModal.jsx
@@ -2,11 +2,11 @@ import Close from '..//icons/Close';
 import {} from './NeedHelpModal.css';
 import { useEffect } from 'react';
 
-const NeedHelpModal = ({ setShowModal }) => {
+const NeedHelpModal = ({ onClose }) => {
   useEffect(() => {
     const handleKeyDown = evt => {
       if (evt.code === 'Escape') {
-        setShowModal(false);
+        onClose();
       }
     };
 
@@ -17,46 +17,50 @@ const NeedHelpModal = ({ setShowModal }) => {
       window.removeEventListener('keydown', handleKeyDown);
       document.body.style.overflow = 'auto';
     };
-  }, [setShowModal]);
+  }, [onClose]);
 
   const handleSubmit = evt => {
     if (evt.target === evt.currentTarget) {
-      setShowModal(false);
+      onClose();
     }
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="help-form">
-        <Close
-          className="close-icon"
-          width={18}
-          height={18}
-          fillColor="#fff"
-          strokeColor="#fff"
-          onClick={() => setShowModal(false)}
-        />
-        <h2 className="form-title-lh">Need Help</h2>
-        <div>
-          <input
-            type="email"
-            name="email"
-            className="input-email"
-            placeholder="Email address"
-          />
+      <div className="modal-overlay">
+        <div className="modal-help">
+          <form onSubmit={handleSubmit} className="help-form">
+            <button type="button" className="close-button" onClick={onClose}>
+              <Close
+                width={18}
+                height={18}
+                fillColor="#fff"
+                strokeColor="#fff"
+              />
+            </button>
+            <h2 className="form-title-lh">Need Help</h2>
+            <div>
+              <input
+                type="email"
+                name="email"
+                className="input-email"
+                placeholder="Email address"
+              />
+            </div>
+            <div className="row-form">
+              <textarea
+                type="text"
+                name="comment"
+                className="comment"
+                placeholder="Comment"
+              />
+            </div>
+            <button type="submit" className="btn-send-lg">
+              Send
+            </button>
+          </form>
         </div>
-        <div className="row-form">
-          <input
-            type="text"
-            name="comment"
-            className="comment"
-            placeholder="Comment"
-          />
-        </div>
-        <button type="submit" className="btn-send-lg">
-          Send
-        </button>
-      </form>
+      </div>
     </>
   );
 };
