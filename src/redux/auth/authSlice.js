6536c37d1be4e8a1ec380da7d77b296cb77df705
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-01 13:15:21 +0200

    create slice/add register, login, logout reducers

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,8 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { register, logIn, logOut } from './authOperations';
 
-const authInitState = {
-  board: {},
+const initialState = {
+  user: { name: null, email: null },
   token: null,
+  board: {},
   refreshToken: null,
   isLoggedIn: false,
   isRefreshing: false,
@@ -11,8 +13,25 @@ const authInitState = {
 
 export const authSlice = createSlice({
   name: 'auth',
-  initialState: authInitState,
-  extraReducers: () => {},
+  initialState,
+  extraReducers: builder => {
+    builder
+      .addCase(register.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        state.token = payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logIn.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        state.token = payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logOut.fulfilled, (state, { payload }) => {
+        state.user = { name: null, email: null };
+        state.token = null;
+        state.isLoggedIn = false;
+      })
+  },
 });
 
 export const authReducer = authSlice.reducer;
