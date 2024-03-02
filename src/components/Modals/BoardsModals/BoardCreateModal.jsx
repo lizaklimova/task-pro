7af62b6abc05a55ccae02becc88309ff8652b080
Elostay <anastasiia.motsukh@automaze.io>
2024-03-02 13:17:05 +0200

    icons bug, adaptive -need to be done

--- src/components/Modals/BoardsModals/BoardCreateModal.jsx
+++ src/components/Modals/BoardsModals/BoardCreateModal.jsx
@@ -3,15 +3,16 @@ import ModalWrapper from '../ModalWrapper/ModalWrapper';
 import { IconsList } from './IconsList';
 import Plus from 'components/Icons/Plus';
 import { Form, Title, Input, Text, Button, Span } from './BoardModals.styled';
+import { AddBtn } from 'components/Sidebar/SidebarContent/SidebarContent.styled';
 
 export const BoardCreateModal = () => {
   const [shownModal, setShownModal] = useState(false);
 
   return (
     <>
-      <button type="button" onClick={() => setShownModal(true)}>
-        Open Modal Board Base
-      </button>
+      <AddBtn type="button" onClick={() => setShownModal(true)}>
+        <Plus width={20} height={20} strokeColor={'var(--sidebar-plus-icon)'} />
+      </AddBtn>
       {shownModal && (
         <ModalWrapper width={350} onClose={() => setShownModal(false)}>
           <Form>
