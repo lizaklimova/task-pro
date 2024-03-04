15184c7a47cdb6e734be25d84ed5749bd44e1f5b
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-04 15:15:14 +0200

    add yup login schema

---
+++ src/Validations/UserLoginValidation.js
@@ -0,0 +1,8 @@
+import * as yup from 'yup';
+
+const loginSchema = yup.object().shape({
+    email: yup.string().email("Please enter a valid email").required("Email is required"),
+    password: yup.string().min(6).max(24).required("Password is required")
+})
+
+export default loginSchema;
