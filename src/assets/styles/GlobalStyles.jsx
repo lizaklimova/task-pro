752c883a8bdb52902b7d9407b83f26ba31bdea23
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2024-03-04 21:33:35 +0200

    post ligic

--- src/assets/styles/GlobalStyles.jsx
+++ src/assets/styles/GlobalStyles.jsx
@@ -49,6 +49,8 @@ html[data-theme='dark']{
   --svg-color-user:#151515;
   --sidebar-logout-text: #fff;
   --sidebar-before: #bedbb0;
+  --default-icon-fill: rgb(31, 31, 31);
+  --default-icon-stroke: rgba(255, 255, 255, 0.1);
 }
 
 html[data-theme='light']{
@@ -95,6 +97,8 @@ html[data-theme='light']{
   --svg-color-user:#fff;
   --sidebar-logout-text: #161616;
   --sidebar-before: #bedbb0;
+  --default-icon-fill: rgba(22, 22, 22, 0.1);
+  --default-icon-stroke: rgb(246, 246, 247);
 }
 
 html[data-theme='violet']{
@@ -141,6 +145,8 @@ html[data-theme='violet']{
   --svg-color-user:#fff;
   --sidebar-logout-text: #fff;
   --sidebar-before: #fff;
+  --default-icon-fill: rgba(22, 22, 22, 0.1);
+  --default-icon-stroke: rgb(236, 237, 253);
 }
 
 *,
--- src/redux/board/boardSlice.js
+++ src/redux/board/boardSlice.js
@@ -1,5 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { getBackgroundIcons } from './boardOperations';
+import {
+  getBackgroundIcons,
+  getAllBoards,
+  createBoard,
+} from './boardOperations';
 import { handlePending, handleRejected } from './helpers';
 
 const boardsSlice = createSlice({
@@ -15,8 +19,26 @@ const boardsSlice = createSlice({
       .addCase(getBackgroundIcons.pending, handlePending)
       .addCase(getBackgroundIcons.fulfilled, (state, action) => {
         state.background = action.payload;
+        state.isLoading = false;
+        state.error = null;
       })
-      .addCase(getBackgroundIcons.rejected, handleRejected);
+      .addCase(getBackgroundIcons.rejected, handleRejected)
+
+      .addCase(getAllBoards.pending, handlePending)
+      .addCase(getAllBoards.fulfilled, (state, action) => {
+        state.boards = action.payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(getAllBoards.rejected, handleRejected)
+
+      .addCase(createBoard.pending, handlePending)
+      .addCase(createBoard.fulfilled, (state, action) => {
+        state.boards.push(action.payload);
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(createBoard.rejected, handleRejected);
   },
 });
 
