a80594f0413ab86df533d070d780ba5b80b11c94
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-05 12:24:21 +0200

    edit error variable for error styles

--- src/components/Auth/RegisterForm/RegisterForm.jsx
+++ src/components/Auth/RegisterForm/RegisterForm.jsx
@@ -78,7 +78,7 @@ const RegisterForm = () => {
             value={values.email}
             onChange={handleChange}
             onBlur={handleBlur}
-            error={errors.email && touched.email}
+            $error={errors.email && touched.email}
             name="email"
             placeholder="Enter your email"
             type="email"
@@ -90,7 +90,7 @@ const RegisterForm = () => {
             value={values.password}
             onChange={handleChange}
             onBlur={handleBlur}
-            error={errors.password && touched.password}
+            $error={errors.password && touched.password}
             name="password"
             placeholder="Create a password"
             type="password"
