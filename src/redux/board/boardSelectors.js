3e957c6519debc0f9460f6d306da4a7473995573
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2024-03-05 15:27:10 +0200

    Liza hands

--- src/redux/board/boardSelectors.js
+++ src/redux/board/boardSelectors.js
@@ -1,3 +1,3 @@
 export const selectBackgroundIcons = state => state.boards.background;
-
 export const selectBoards = state => state.boards.boards;
+export const selectIsLoading = state => state.boards.isLoading;
