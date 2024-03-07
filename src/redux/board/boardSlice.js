91e988ad23e156a3c9ff943092624bcd3da4602a
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2024-03-07 22:22:07 +0200

    com

--- src/redux/board/boardSlice.js
+++ src/redux/board/boardSlice.js
@@ -58,6 +58,7 @@ const boardsSlice = createSlice({
         // state.oneBoard.title = payload.title;
         // state.oneBoard.backgroundId = payload.backgroundId;
         // state.oneBoard.iconId = payload.iconId;
+        console.log(payload);
         state.oneBoard = { ...state.oneBoard, ...payload };
         state.boards = state.boards.map(board =>
           board._id === payload._id ? payload : board
