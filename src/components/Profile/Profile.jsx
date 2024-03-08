12f3e12fde7aa7f5049e1f484285f13037a143be
Valentyna Dumbrava <valyamoldovan55555@outlook.com>
2024-03-08 12:08:23 +0200

    edit avatar

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,35 +1,46 @@
 import React, { useState } from 'react';
-// import { useAuth } from 'hooks';
 import ModalWrapper from 'components/Modals/ModalWrapper';
 import UserModal from 'components/Modals/UserModal/UserModal';
 import { Button, UserContainer, UserName } from './Profile.styled';
 import UserDefault from 'components/Icons/UserDefault';
 import { useSelector } from 'react-redux';
-import { selectUsername } from '../../redux/auth/authSelectors';
+import {
+  selectUserAvatar,
+  selectUsername,
+  selectIsLoading,
+} from '../../redux/auth/authSelectors';
 
 const Profile = () => {
   const [shownModal, setShownModal] = useState(false);
   const nameUser = useSelector(selectUsername);
-  // const { username } = useAuth();
+  const avatar = useSelector(selectUserAvatar);
+  const isLoading = useSelector(selectIsLoading);
 
   return (
-    <UserContainer>
-      <UserName>{nameUser ?? 'User'}</UserName>
-      <Button type="button" onClick={() => setShownModal(true)}>
-        <UserDefault
-          width={68}
-          height={68}
-          strokeColor={'var( --svg-back-color-user)'}
-          fillColor={'var(--svg-color-user)'}
-        />
-        {/* <img src="#" alt="" /> */}
-      </Button>
-      {shownModal && (
-        <ModalWrapper width={400} onClose={() => setShownModal(false)}>
-          <UserModal />
-        </ModalWrapper>
+    <>
+      {!isLoading && (
+        <UserContainer>
+          <UserName>{nameUser ?? 'User'}</UserName>
+          <Button type="button" onClick={() => setShownModal(true)}>
+            {avatar !== 'default' ? (
+              <img src={avatar} alt="" />
+            ) : (
+              <UserDefault
+                width={68}
+                height={68}
+                strokeColor={'var( --svg-back-color-user)'}
+                fillColor={'var(--svg-color-user)'}
+              />
+            )}
+          </Button>
+          {shownModal && (
+            <ModalWrapper width={400} onClose={() => setShownModal(false)}>
+              <UserModal onClose={() => setShownModal(false)} />
+            </ModalWrapper>
+          )}
+        </UserContainer>
       )}
-    </UserContainer>
+    </>
   );
 };
 
