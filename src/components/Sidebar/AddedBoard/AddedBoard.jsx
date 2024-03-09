4a8a7c37caffa2b5d711bbda688f19845009f768
Elizabeth Klymova <lozaklimovaaadd@gmail.com>
2024-03-09 15:08:15 +0200

    fixed linter warning

--- src/components/Sidebar/AddedBoard/AddedBoard.jsx
+++ src/components/Sidebar/AddedBoard/AddedBoard.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { deleteBoard } from '../../../redux/board/boardOperations';
 import { ICONS_ARRAY } from 'constants';
@@ -18,7 +18,7 @@ const AddedBoard = ({ allBoards, board, openEditModal }) => {
   const boardIcon = ICONS_ARRAY[board.icon_id];
   const [isDeleteModalShown, setIsDeleteModalShown] = useState(false);
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const handleBoardDelete = () => {
